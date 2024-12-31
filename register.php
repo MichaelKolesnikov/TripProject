66eb1926c76c1e6d13383c3f85e1db11ffab76af
MichaelKolesnikov <michaelkolesnikov056@mail.ru>
2024-12-31 16:34:39 +0300

    first commit

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+    <form>
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <label>Подтверждение пароля</label>
+        <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+        <label>Как к вам обращаться?</label>
+        <input type="text" name="name" placeholder="Введите свое имя">
+        <label>Телефон</label>
+        <input type="phone" name="phone" placeholder="Введите номер телефона">
+
+        <button type="submit" class="register-btn">Зарегистрироваться</button>
+        <p>
+            У вас уже есть аккаунт? - <a href="/">авторизируйтесь</a>!
+        </p>
+        <p class="msg none">Lorem ipsum.</p>
+    </form>
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
